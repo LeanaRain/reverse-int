bc2e8327c74383ccdd5447e38c57f2f0bb351bc8
LeanaRain <137782467+LeanaRain@users.noreply.github.com>
2024-08-03 01:41:54 +0500

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    let newArr = String(n).split(""); // преобразуем номер в строку и разбиваем ее на массив
+    let newString = newArr.reverse().join(""); // массив в обратном порядке соединяем, по умолч. join имеет раздлитель запятую, меняю на пуст. строку.
+    return parseInt(newString);
 }
